07eae7eaf5193d161c8479f71d2915068bb380f0
Janne Pohjolainen <janne.pohjolainen@polarsquad.com>
2022-09-15 15:56:16 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,34 @@
+# Wundernut Vol. 12
+
+This is a small program to solve the Wundernut vol. 12.
+
+Wundernut vol. 12 can be found at https://github.com/wunderdogsw/wundernut-vol12
+
+Download `parchment.png` from the Wundernut vol12 github.
+
+## Installation
+
+This program requires `tesseract` binary to be installed.
+
+Ubuntu:
+```shell
+
+$ apt-get install tesseract-ocr
+```
+Create virtual env and install required Python libraries:
+
+```shell
+
+$ python3.8 -m venv venv
+
+$ . venv/bin/activate
+
+$ pip3 install -r requirements.txt
+```
+
+## Usage
+
+```shell
+
+$ ./nut.py parchment.png
+```
---
+++ nut.py
@@ -0,0 +1,63 @@
+#!python3
+####
+# Program to solve a puzzle of Wundernut vol. 12
+#
+
+import argparse
+import wordninja
+
+from PIL import Image
+from pytesseract import image_to_string
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Wondernut-vol12')
+    parser.add_argument('imagefile')
+    args = parser.parse_args()
+
+    print(f"Reading '{args.imagefile}'...")
+    im = Image.open(args.imagefile)
+
+    width = im.size[0]
+    height = im.size[1]
+
+    print('Mangling colors...')
+    bg = im.getpixel((0,0)) # background color from first pixel
+    for x in range(0, width):
+        for y in range(0, height):
+            if bg == im.getpixel((x, y)): # Turn background color to white
+                im.putpixel((x, y), (255, 255, 255))
+            else: # If deviates from bg color, change to black
+                im.putpixel((x, y), (0, 0, 0))
+
+    print(f"Resizing {width}x{height} to {width//3}x{height//2}...")
+    im_res = im.resize((width//3, height//2))
+    im.close() # close original image
+
+    print('Reading text from image...')
+    conf = "--psm 6"
+    encoded_text = image_to_string(im_res, config=conf)
+    print('Text found:')
+    print(encoded_text)
+
+    print('Decoding...')
+    # Scrolling through alphabet range and rotate each letter by the number
+    # forward. wrap around alphabets: Z->A
+    for x in range(27):
+        decoded = ''
+        for c, i in enumerate(encoded_text):
+            if i not in ['\n', '\r']:
+                # Only circle around capital letters and not whole ascii table
+                o = 65 + ((ord(i) + x) - 65) % 26
+                decoded += chr(o)
+        if 'THE' in decoded: # Find english text
+            print(f"Using ROT{x} found a match:\n")
+            sp = wordninja.split(decoded)
+            # print in 10 words per line
+            for i in range(0, len(sp), 10):
+                print(' '.join(sp[i:i+10]))
+            break
+
+
+if __name__ == '__main__':
+    main()
